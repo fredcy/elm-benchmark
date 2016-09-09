9ff483f420c95319648669b5e81043535036f29e
Fred Yankowski <fcy@imsa.edu>
2016-09-09 11:28:11 -0500

    Separate out driver program

--- example/Main.elm
+++ example/Main.elm
@@ -1,166 +1,22 @@
 module Main exposing (main)
 
-import Html exposing (Html)
-import Html.App
-import Html.Attributes as HA
 import Benchmark
-import Process
-import Task
-import Numeral
+import Program
+import Html.App
 
 
-type alias Model =
-    { results : List Benchmark.Result
-    , done : Bool
-    , platform : Maybe String
-    , errors : List Benchmark.ErrorInfo
-    }
-
-
-type Msg
-    = Started ()
-    | Event Benchmark.Event
-
-
+main : Program Never
 main =
-    Html.App.program
-        { init = init
-        , update = update
-        , view = view
-        , subscriptions = always (Benchmark.events Event)
-        }
-
-
-init : ( Model, Cmd Msg )
-init =
-    ( Model [] False Nothing []
-    , Task.perform (\_ -> Debug.crash "Benchmark.runTask failed")
-        Started
-        (Benchmark.runTask
-            [ suiteN 10
-            , suiteN 1000
-            , suiteN 100000
-            , suiteN 1000000
-            ]
-        )
-    )
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        Event event ->
-            case event of
-                Benchmark.Start { platform } ->
-                    { model | platform = Just platform } ! []
-
-                Benchmark.Cycle result ->
-                    { model | results = model.results ++ [ result ] } ! []
-
-                Benchmark.Complete result ->
-                    model ! []
-
-                Benchmark.Finished ->
-                    { model | done = True } ! []
-
-                Benchmark.BenchError error ->
-                    { model | errors = model.errors ++ [ error ] } ! []
-
-        _ ->
-            model ! []
-
-
-view : Model -> Html Msg
-view model =
-    Html.div []
-        [ Html.h1 [] [ Html.text "Benchmark results" ]
-        , viewPlatform model.platform
-        , viewResults model
-        , viewErrors model.errors
-        , viewStatus model
+    -- Run over range of sizes that should include some stack-overflow cases.
+    Program.program
+        [ suiteN 10
+        , suiteN 1000
+        , suiteN 100000
+        , suiteN 1000000
         ]
 
 
-viewPlatform platformMaybe =
-    case platformMaybe of
-        Just platform ->
-            Html.div []
-                [ Html.h2 [] [ Html.text "Platform" ]
-                , Html.p [] [ Html.text platform ]
-                ]
-
-        Nothing ->
-            Html.text ""
-
-
-viewStatus : Model -> Html Msg
-viewStatus model =
-    Html.p []
-        [ Html.text
-            (if model.done then
-                "Done"
-             else
-                "running ..."
-            )
-        ]
-
-
-viewResults : Model -> Html Msg
-viewResults model =
-    let
-        viewResult e =
-            if e.samples == [] then
-                Nothing
-            else
-                let
-                    stats =
-                        Benchmark.getStats e.samples
-
-                    meanFreq =
-                        1 / stats.mean
-                in
-                    Just
-                        (Html.tr
-                            []
-                            [ Html.td [] [ Html.text e.suite ]
-                            , Html.td [] [ Html.text e.benchmark ]
-                            , Html.td [ HA.class "numeric" ] [ Html.text (Numeral.format "0" meanFreq) ]
-                            , Html.td [ HA.class "numeric" ] [ Html.text (Numeral.format "0.0" stats.relativeMarginOfError) ]
-                            , Html.td [ HA.class "numeric" ] [ Html.text (toString stats.size) ]
-                            ]
-                        )
-
-        th str =
-            Html.th [] [ Html.text str ]
-    in
-        Html.table []
-            [ Html.thead []
-                (List.map th [ "suite", "benchmark", "ops/sec", "error%", "samples" ])
-            , Html.tbody []
-                (List.filterMap viewResult model.results)
-            ]
-
-
-viewErrors : List Benchmark.ErrorInfo -> Html Msg
-viewErrors errors =
-    if errors == [] then
-        Html.text ""
-    else
-        Html.div []
-            [ Html.h2 [] [ Html.text "Errors" ]
-            , Html.ul [] (List.map viewError errors)
-            ]
-
-
-viewError : Benchmark.ErrorInfo -> Html Msg
-viewError error =
-    Html.li [] [ Html.text (toString error) ]
-
-
-
--- benchmarks and their suites
-
-
+{-| Set shorter benchmark time to make it complete more quickly. -}
 options =
     let
         defaults =
@@ -169,21 +25,25 @@ options =
         { defaults | maxTime = 2 }
 
 
+{-| Run the benchmark functions over list of given size.
+-}
 suiteN size =
     let
         testdata =
             [1..size]
 
-        benchFn name fn =
-            Benchmark.bench name (\() -> fn 0 testdata)
+        makeBench name foldfn =
+            Benchmark.bench name (\() -> foldfn (\x s -> s + x) 0 testdata)
     in
         Benchmark.suiteWithOptions options
             ("size " ++ toString size)
-            [ benchFn "List.foldl" (List.foldl (\x s -> s + x))
-            , benchFn "new foldl" (newFoldl (\x s -> s + x))
+            [ makeBench "List.foldl" List.foldl
+            , makeBench "new foldl" newFoldl
             ]
 
 
+{-| Naive version of foldl that grows the call stack.
+-}
 newFoldl : (a -> b -> b) -> b -> List a -> b
 newFoldl fn initial vals =
     case vals of
---
+++ src/Program.elm
@@ -0,0 +1,164 @@
+module Program exposing (program)
+
+{-| Provide a driver program that runs a list of benchmark suites and reports
+the results.
+
+This is an example of the Benchmark API and can be used as a default benchmark
+driver.
+
+@doc program
+-}
+
+import Html exposing (Html)
+import Html.App
+import Html.Attributes as HA
+import Benchmark
+import Task
+import Numeral
+
+
+type alias Model =
+    { results : List Benchmark.Result
+    , done : Bool
+    , platform : Maybe String
+    , errors : List Benchmark.ErrorInfo
+    }
+
+
+type Msg
+    = Started ()
+    | Event Benchmark.Event
+
+
+{-| Create driver program from list of suites.
+-}
+program : List Benchmark.Suite -> Program Never
+program suites =
+    Html.App.program
+        { init = init suites
+        , update = update
+        , view = view
+        , subscriptions = always (Benchmark.events Event)
+        }
+
+
+init : List Benchmark.Suite -> ( Model, Cmd Msg )
+init suites =
+    ( Model [] False Nothing []
+    , Task.perform (\_ -> Debug.crash "Benchmark.runTask failed")
+        Started
+        (Benchmark.runTask suites)
+    )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Event event ->
+            case event of
+                Benchmark.Start { platform } ->
+                    { model | platform = Just platform } ! []
+
+                Benchmark.Cycle result ->
+                    { model | results = model.results ++ [ result ] } ! []
+
+                Benchmark.Complete result ->
+                    model ! []
+
+                Benchmark.Finished ->
+                    { model | done = True } ! []
+
+                Benchmark.BenchError error ->
+                    { model | errors = model.errors ++ [ error ] } ! []
+
+        _ ->
+            model ! []
+
+
+view : Model -> Html Msg
+view model =
+    Html.div []
+        [ Html.h1 [] [ Html.text "Benchmark results" ]
+        , viewPlatform model.platform
+        , viewResults model
+        , viewErrors model.errors
+        , viewStatus model
+        ]
+
+
+viewPlatform platformMaybe =
+    case platformMaybe of
+        Just platform ->
+            Html.div []
+                [ Html.h2 [] [ Html.text "Platform" ]
+                , Html.p [] [ Html.text platform ]
+                ]
+
+        Nothing ->
+            Html.text ""
+
+
+viewStatus : Model -> Html Msg
+viewStatus model =
+    Html.p []
+        [ Html.text
+            (if model.done then
+                "Done"
+             else
+                "running ..."
+            )
+        ]
+
+
+viewResults : Model -> Html Msg
+viewResults model =
+    let
+        viewResult e =
+            if e.samples == [] then
+                -- means the benchmark errored out
+                Nothing
+            else
+                let
+                    stats =
+                        Benchmark.getStats e.samples
+
+                    meanFreq =
+                        -- convert mean-period (seconds) to mean-frequency (ops/sec)
+                        1 / stats.mean
+                in
+                    Just
+                        (Html.tr
+                            []
+                            [ Html.td [] [ Html.text e.suite ]
+                            , Html.td [] [ Html.text e.benchmark ]
+                            , Html.td [ HA.class "numeric" ] [ Html.text (Numeral.format "0" meanFreq) ]
+                            , Html.td [ HA.class "numeric" ] [ Html.text (Numeral.format "0.0" stats.relativeMarginOfError) ]
+                            , Html.td [ HA.class "numeric" ] [ Html.text (toString stats.size) ]
+                            ]
+                        )
+
+        th str =
+            Html.th [] [ Html.text str ]
+    in
+        Html.table []
+            [ Html.thead []
+                (List.map th [ "suite", "benchmark", "ops/sec", "error%", "samples" ])
+            , Html.tbody []
+                (List.filterMap viewResult model.results)
+            ]
+
+
+viewErrors : List Benchmark.ErrorInfo -> Html Msg
+viewErrors errors =
+    if errors == [] then
+        Html.text ""
+    else
+        Html.div []
+            [ Html.h2 [] [ Html.text "Errors" ]
+            , Html.ul [] (List.map viewError errors)
+            ]
+
+
+viewError : Benchmark.ErrorInfo -> Html Msg
+viewError error =
+    Html.li [] [ Html.text (toString error) ]
